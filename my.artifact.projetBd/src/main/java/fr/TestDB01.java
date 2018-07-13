0096eb4c4e38777a39b0ba47abcb9347a8571cc5
aure <mouretaurelien95git@gmail.com>
2018-07-13 14:14:34 +0200

    exo 11 started mysql dependencies added

--- my.artifact.projetBd/src/main/java/fr/TestDB01.java
+++ my.artifact.projetBd/src/main/java/fr/TestDB01.java
@@ -19,9 +19,9 @@ public class TestDB01 {
 	public static void main(String[] args) {
 
 		final String dbDriver = "com.mysql.jdbc.Driver";
-		final String dbUrl = "http://localhost/phpmyadmin/db_structure.php?server=1&db=banque";
+		final String dbUrl = "jdbc:mysql://localhost/banque";
 		final String dbLogin = "root";
-		final String dbPwd = "root";
+		final String dbPwd = "";
 
 		Connection connection = null;
 		Statement request = null;
@@ -39,6 +39,32 @@ public class TestDB01 {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
+		try {
+			request = connection.createStatement();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			// e.printStackTrace();
+		}
+
+		try {
+			resultat = request.executeQuery("select * from utilisateur");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			// e.printStackTrace();
+		}
+
+		try {
+			while (resultat.next()) {
+				System.out.println(resultat.getString("nom"));
+				System.out.println(resultat.getString("prenom"));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			// e.printStackTrace();
+		}
+
+
 
 	}
 
